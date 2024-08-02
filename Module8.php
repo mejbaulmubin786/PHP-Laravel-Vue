0b3d5878d849ebc4dd071f82b63a7671b8f4016c
Nowshen Anjuman Khan Prapte <nowshen.anjum@gmail.com>
2024-08-02 19:17:31 +0600

    "add new module 8 problem solve"

--- Module8.php
+++ Module8.php
@@ -13,8 +13,61 @@ Output: aples
 Solved in PHP:
 */
 
-$string = fgets(STDIN);                 // Taken string input
-$array = str_split($string);            // String split into an array
-$rmDuplicate = array_unique($array);    // removing duplicate  characters
-$output = join($rmDuplicate);           // joined as a string
-print($output);                         // print output
+# Taken string input
+$string = fgets(STDIN);
+# String split into an array               
+$array = str_split($string);
+# removing duplicate  characters        
+$rmDuplicate = array_unique($array);
+# joined as a string 
+$output = join($rmDuplicate);
+# print output       
+print($output);
+
+
+
+/*
+2. Check Anagram
+Problem Statement: You are given two strings A and B. Your task is check whether the string B is anagram of string A or not. An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, “abcd” and “dabc” are an anagram of each other whereas "abcd" and "aabc" are not.
+
+Input: The input consists of two strings A and B in two lines containing only lowercase English letters.
+
+Output: In output print "YES" if string B is anagram of string A or "NO" otherwise. Print without quotation mark.
+
+Constraints: 1 ≤ ∣A∣,∣B∣ ≤ 10000
+strings contains only lowercase (a−z) English letters.
+
+Example 1:
+    Input: abcd , dbac
+    Output: YES
+
+Example 2:
+    Input: abcd , aabc
+    Output: NO
+
+    */
+
+#Function checking A and B is anagram or not.
+function anagram($A, $B)
+{
+    if (strlen($A) != strlen($B)) {
+        return "No";
+    }
+
+    $arrA = str_split($A);
+    $arrB = str_split($B);
+    sort($arrA);
+    sort($arrB);
+    if ($arrA === $arrB) {
+        return "YES";
+    } else {
+        return "NO";
+    }
+}
+
+#Taken string input
+$A = trim(fgets(STDIN));
+$B = trim(fgets(STDIN));
+
+#Print output
+echo anagram($A, $B);

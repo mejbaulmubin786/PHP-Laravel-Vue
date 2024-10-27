2a9679f803ccf5b7bf233f9f8ea7e4af10b3ca43
Nowshen Anjuman Khan Prapte <nowshen.anjum@gmail.com>
2024-10-28 00:26:08 +0600

    "Updated README.md and added module-19"

---
+++ module-19/LoneZeroes.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Lone Zeroes
+Problem Statement: Write a program to create a function which counts how many lone 0s appear in a given number. Lone means the number doesn't appear twice or more in a row.Previous and next numbers are not zeroes. For example - countLoneZeroes(10101) ➞ 2
+Input: The program will take an integer NN as input
+Output: The output will print the number of lone 0's in the integer.
+Constraints: 0 ≤ N ≤ 100000
+Example: Enter number
+Input: 10101
+Output: 2
+Notes: None
+*/
+
